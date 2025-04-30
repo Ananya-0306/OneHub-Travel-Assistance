d907ca22382e52df2bbbf7dc8990f9466dcaf60e
Ananya-0306 <ananyachatterjee495@gmail.com>
2025-04-30 11:44:33 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import RideSuggestions from "./components/RideSuggestions";
-import Wallet from "./pages/Wallet";
-import RideHistory from "./pages/RideHistory";
-
-<Route path="/ride-history" element={<RideHistory />} />
-
 
 const App = () => {
-  const [walletBalance, setWalletBalance] = useState(0);
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home walletBalance={walletBalance} />} />
+        <Route path="/" element={<Home />} />
         <Route path="/ride-suggestion" element={<RideSuggestions />} />
-        <Route path="/wallet" element={<Wallet onBalanceUpdate={setWalletBalance} />} />
       </Routes>
     </Router>
   );
